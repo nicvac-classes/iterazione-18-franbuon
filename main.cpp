5c655a9c54108388ad1efa6131c4147b31546551
franbuon <francescogiuseppe.buono@marconibari.edu.it>
2024-11-09 09:06:26 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,34 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    int i;
+    float livello, somma, media;
+    somma=0;
+    i=1;
+    while(i<=7)
+    {
+        cout<<"A quanto arriva il livello dell'acqua?"<<endl;
+        cin>>livello;
+        somma=somma+livello;
+        if(somma>10)
+        {
+            cout<<"Il livello dell'acqua supera i 10cm"<<endl;
+        }
+        else
+        {
+            cout<<"Il livello dell' acqua non supera i 10cm"<<endl;
+        }
+        i=i+1;
+    }
+    media=somma/7;
+    if(media>0)
+    {
+        cout<<"Acqua alta";
+    }
+    else
+    {
+        cout<<"Livello normale";
+    }
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
